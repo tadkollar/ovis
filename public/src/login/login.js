da1e5b298b5b728c9c0c6bedffcbb2d5311c798f
RyanFarr01 <ryan.farr@nasa.gov>
2017-08-08 15:09:00 -0400

    Modal mostly working

--- public/src/login/login.js
+++ public/src/login/login.js
@@ -1,14 +1,28 @@
 
-var register = function() {
+var register = function () {
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
 
-    http.post('token', {'name': name, 'email': email}, function(response) {
+    http.post('token', { 'name': name, 'email': email }, function (response) {
         console.log(response);
+        if (response.status === 'Success') {
+            $('#registerSuccessBody').show();
+            $('#registerFailBody').hide();
+            $('#registerSuccessClose').hide();
+            $('#registerSuccessOkay').show();
+            $('#tokenText').text(response.token);
+        }
+        else {
+            $('#registerSuccessBody').hide();
+            $('#registerFailBody').show();
+            $('#registerSuccessClose').show();
+            $('#registerSuccessOkay').hide();
+            $('#failedText').text(response.reasoning);
+        }
     });
 };
 
-var login = function() {
+var login = function () {
     var token = document.getElementById('token').value;
 
     //Send to server, maybe redirect?
