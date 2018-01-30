2e0367bde6e63098e322bd217e4f9da53fafa268
ryanfarr <ryanfarr01@gmail.com>
2018-01-30 11:44:06 -0500

    Added a shell build script for electron. Very similar to the batch script

---
+++ build_electron.sh
@@ -0,0 +1,3 @@
+electron-packager . OpenMDAOVis --platform=win32 --arch=x64 --overwrite
+electron-packager . OpenMDAOVis --platform=linux --overwrite
+electron-packager . OpenMDAOVis --platform=mas --overwrite
