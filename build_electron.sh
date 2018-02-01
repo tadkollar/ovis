42fe131f60059d6844f32aea6ca4e06497050e40
ryanfarr <ryanfarr01@gmail.com>
2018-02-01 16:27:15 -0500

    Added install scripts and updated build script

--- build_electron.sh
+++ build_electron.sh
@@ -1,3 +1,10 @@
-electron-packager . OVis --platform=win32 --arch=x64 --overwrite
+ electron-packager . OVis --platform=win32 --arch=x64 --overwrite
 electron-packager . OVis --platform=linux --overwrite
 electron-packager . OVis --platform=mas --overwrite
+python setup.py bdist_wheel
+cp dist/ZuneServerReqs-1.0-py2.py3-none-any.whl OVis-linux-x64
+cp dist/ZuneServerReqs-1.0-py2.py3-none-any.whl OVis-mas-x64
+cp dist/ZuneServerReqs-1.0-py2.py3-none-any.whl OVis-win32-x64
+cp install_server_requirements.sh OVis-linux-x64
+cp install_server_requirements.sh OVis-mas-x64
+cp install_server_requirements.cmd OVis-win32-x64
---
+++ install_server_requirements.cmd
@@ -0,0 +1,1 @@
+pip install ./ZuneServerReqs-1.0-py2.py3-none-any.whl
