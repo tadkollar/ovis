69a3079053be30261143e40f12ab5e28771a177c
openmdao <openmdao@openmdaombp.ndc.nasa.gov>
2018-02-16 14:52:07 -0500

    Forgot to add changes to build_electron.cmd

--- build_electron.cmd
+++ build_electron.cmd
@@ -1,3 +1,1 @@
-start electron-packager . OVis --platform=win32 --arch=x64 --overwrite
-start electron-packager . OVis --platform=linux --overwrite
-start electron-packager . OVis --platform=mas --overwrite
+npm run dist
