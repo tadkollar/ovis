a93af3471b7c249bb7a33d50ea8a8e265a20e12b
openmdao <openmdao@openmdaombp.ndc.nasa.gov>
2018-02-20 16:58:11 -0500

    Change layout back to old method. Will fix for mac later

--- main.js
+++ main.js
@@ -108,122 +108,122 @@ function findFile() {
 
 
 
-// const template = [
-//   {
-//     label: 'File',
-//     submenu: [
-//       {
-//         role: 'open',
-//         label: 'Open',
-//         accelerator: 'CmdOrCtrl+o',
-//         click() {
-//           findFile();
-//         }
-//       }
-//     ]
-//   },
-//   {
-//     label: 'View',
-//     submenu: [
-//       {
-//         accelerator: 'CmdOrCtrl+Shift+i',
-//         role: 'toggle dev tools',
-//         label: 'Toggle Developer Tools',
-//         click() { mainWindow.webContents.openDevTools(); }
-//       },
-//       {
-//         accelerator: 'CmdOrCtrl+r',
-//         role: 'refresh',
-//         label: 'Refresh Page',
-//         click() { mainWindow.webContents.reloadIgnoringCache(); }
-//       }
-//     ]
-//   }
-// ]
 const template = [
   {
-    label: 'Edit',
+    label: 'File',
     submenu: [
-      { role: 'undo' },
-      { role: 'redo' },
-      { type: 'separator' },
-      { role: 'cut' },
-      { role: 'copy' },
-      { role: 'paste' },
-      { role: 'pasteandmatchstyle' },
-      { role: 'delete' },
-      { role: 'selectall' }
+      {
+        role: 'open',
+        label: 'Open',
+        accelerator: 'CmdOrCtrl+o',
+        click() {
+          findFile();
+        }
+      }
     ]
   },
   {
     label: 'View',
     submenu: [
-      { role: 'reload' },
-      { role: 'forcereload' },
-      { role: 'toggledevtools' },
-      { type: 'separator' },
-      { role: 'resetzoom' },
-      { role: 'zoomin' },
-      { role: 'zoomout' },
-      { type: 'separator' },
-      { role: 'togglefullscreen' }
-    ]
-  },
-  {
-    role: 'window',
-    submenu: [
-      { role: 'minimize' },
-      { role: 'close' }
-    ]
-  },
-  {
-    role: 'help',
-    submenu: [
       {
-        label: 'Learn More',
-        click() { require('electron').shell.openExternal('https://electronjs.org') }
+        accelerator: 'CmdOrCtrl+Shift+i',
+        role: 'toggle dev tools',
+        label: 'Toggle Developer Tools',
+        click() { mainWindow.webContents.openDevTools(); }
+      },
+      {
+        accelerator: 'CmdOrCtrl+r',
+        role: 'refresh',
+        label: 'Refresh Page',
+        click() { mainWindow.webContents.reloadIgnoringCache(); }
       }
     ]
   }
 ]
+// const template = [
+//   {
+//     label: 'Edit',
+//     submenu: [
+//       { role: 'undo' },
+//       { role: 'redo' },
+//       { type: 'separator' },
+//       { role: 'cut' },
+//       { role: 'copy' },
+//       { role: 'paste' },
+//       { role: 'pasteandmatchstyle' },
+//       { role: 'delete' },
+//       { role: 'selectall' }
+//     ]
+//   },
+//   {
+//     label: 'View',
+//     submenu: [
+//       { role: 'reload' },
+//       { role: 'forcereload' },
+//       { role: 'toggledevtools' },
+//       { type: 'separator' },
+//       { role: 'resetzoom' },
+//       { role: 'zoomin' },
+//       { role: 'zoomout' },
+//       { type: 'separator' },
+//       { role: 'togglefullscreen' }
+//     ]
+//   },
+//   {
+//     role: 'window',
+//     submenu: [
+//       { role: 'minimize' },
+//       { role: 'close' }
+//     ]
+//   },
+//   {
+//     role: 'help',
+//     submenu: [
+//       {
+//         label: 'Learn More',
+//         click() { require('electron').shell.openExternal('https://electronjs.org') }
+//       }
+//     ]
+//   }
+// ]
 
-if (process.platform === 'darwin') {
-  template.unshift({
-    label: app.getName(),
-    submenu: [
-      // { role: 'about' },
-      { type: 'separator' },
-      { role: 'services', submenu: [] },
-      { type: 'separator' },
-      { role: 'hide' },
-      { role: 'hideothers' },
-      { role: 'unhide' },
-      { type: 'separator' },
-      { role: 'quit' }
-    ]
-  })
+// if (process.platform === 'darwin') {
+//   template.unshift({
+//     label: app.getName(),
+//     submenu: [
+//       // { role: 'about' },
+//       { type: 'separator' },
+//       { role: 'services', submenu: [] },
+//       { type: 'separator' },
+//       { role: 'hide' },
+//       { role: 'hideothers' },
+//       { role: 'unhide' },
+//       { type: 'separator' },
+//       { role: 'quit' }
+//     ]
+//   })
 
-  // Edit menu
-  template[1].submenu.push(
-    { type: 'separator' },
-    {
-      label: 'Speech',
-      submenu: [
-        { role: 'startspeaking' },
-        { role: 'stopspeaking' }
-      ]
-    }
-  )
+//   // Edit menu
+//   template[1].submenu.push(
+//     { type: 'separator' },
+//     {
+//       label: 'Speech',
+//       submenu: [
+//         { role: 'startspeaking' },
+//         { role: 'stopspeaking' }
+//       ]
+//     }
+//   )
 
-  // Window menu
-  template[3].submenu = [
-    { role: 'close' },
-    { role: 'minimize' },
-    { role: 'zoom' },
-    { type: 'separator' },
-    { role: 'front' }
-  ]
-}
+//   // Window menu
+//   template[3].submenu = [
+//     { role: 'close' },
+//     { role: 'minimize' },
+//     { role: 'zoom' },
+//     { type: 'separator' },
+//     { role: 'front' }
+//   ]
+// }
 
 ipcMain.on('openFile', (event, arg) => {
   mainWindow.webContents.send('test', ['blah'])
